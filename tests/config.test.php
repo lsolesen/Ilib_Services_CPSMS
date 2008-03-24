18015359c33ee649ed68e60a0d2318c81dd46d10
Lars Olesen <lars@intraface.dk>
2008-03-24 10:28:40 +0000

    Library to communicate with CPSMS

---
+++ tests/config.test.php
@@ -0,0 +1,6 @@
+<?php
+
+set_include_path(dirname(__FILE__) . '/../src/' . PATH_SEPARATOR . get_include_path());
+define('ILIB_SERVICES_CPSMS_TEST_USERNAME', 'lsolesen');
+define('ILIB_SERVICES_CPSMS_TEST_PASSWORD', 'UDSLNA');
+define('ILIB_SERVICES_CPSMS_TEST_PHONE', '26176860');
