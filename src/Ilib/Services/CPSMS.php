673eb4509ee4aaabcde0e00a9978242a0db59982
Lars Olesen <lars@intraface.dk>
2008-03-25 06:31:19 +0000

    Small updates to the description.

--- src/Ilib/Services/CPSMS.php
+++ src/Ilib/Services/CPSMS.php
@@ -1,19 +1,17 @@
 <?php
 /**
  * Class for easy use of CPSMS gateway.
- * 
+ *
  * PHP Version 5
- * 
+ *
  * @category Services
- * @package Ilib_Services_CPSMS
+ * @package  Ilib_Services_CPSMS
  * @author   Sune Jensen <sj@sunet.dk>
  * @author   Lars Olesen <lars@legestue.net>
- * 
  */
 
 /**
  * Class for easy use of CPSMS gateway.
- *
  *
  * <code>
  * $sms = new Ilib_Services_CPSMS('username', 'password', 'sendername');
@@ -27,7 +25,7 @@
  * @package  Ilib_Services_CPSMS
  * @author   Sune Jensen <sj@sunet.dk>
  * @author   Lars Olesen <lars@legestue.net>
- * 
+ *
  */
 class Ilib_Services_CPSMS
 {
@@ -38,7 +36,7 @@ class Ilib_Services_CPSMS
 
     /**
      * constructor
-     * 
+     *
      * @param string username
      * @param string password
      * @param string sendername
@@ -49,14 +47,14 @@ class Ilib_Services_CPSMS
         $this->url .= "?username=" . $username; // Username
         $this->url .= "&password=" . $password; // Password
         $this->url .= "&from=" . urlencode($sendername); // Sendername
-        
+
         $this->recipient = array();
         $this->errormessage = '';
     }
-    
+
     /**
      * Sets the message to be send
-     * 
+     *
      * @param string message Maximum 459 characters
      * @return boolean true or false
      */
@@ -66,19 +64,19 @@ class Ilib_Services_CPSMS
             $this->setErrorMessage('The message is empty');
             return false;
         }
-        
+
         if(strlen($message) > 459) {
             $this->setErrorMessage('The message is to long. Only 459 characters is allowed.');
             return false;
         }
-        
+
         $this->message = urlencode($message);
         return true;
     }
-    
+
     /**
      * Adds recipients to sms
-     * 
+     *
      * @param string recipient Only 8 numeric characters
      * @return boolean true or false
      */
@@ -88,14 +86,14 @@ class Ilib_Services_CPSMS
             $this->setErrorMessage('Invalid recepient. The number is not 8 numerix characters');
             return false;
         }
-        
+
         $this->recipient[] = $recipient;
         return true;
     }
 
     /**
      * Send sms
-     * 
+     *
      * @return boolean true on success
      */
     public function send()
@@ -104,14 +102,14 @@ class Ilib_Services_CPSMS
             $this->setErrorMessage('The message is empty.');
             return false;
         }
-        
+
         if(!is_array($this->recipient) || empty($this->recipient)) {
             $this->setErrorMessage('No recipients is given.');
             return false;
         }
-        
+
         $send = "&message=" . $this->message;
-        
+
         if(count($this->recipient) > 1 ) {
             foreach($this->recipient AS $recipient) {
                 $send .= "&recipient[]=" . $recipient; // Recipient
@@ -120,7 +118,7 @@ class Ilib_Services_CPSMS
         else {
             $send .= "&recipient=" . $this->recipient[0]; // Recipient
         }
-        
+
         // The url is opened
         $reply = file_get_contents($this->url.$send);
         if (strstr($reply, "<succes>")) {
@@ -130,21 +128,21 @@ class Ilib_Services_CPSMS
             throw new Exception("The message has NOT been sent. Server response: ".$reply);
         }
     }
-    
+
     /**
      * Sets an error message on error
-     * 
+     *
      * @param string error message
      * @return void
      */
-    protected function setErrorMessage($message) 
+    protected function setErrorMessage($message)
     {
         $this->errormessage = $message;
     }
-    
+
     /**
      * Returns an error message after error
-     * 
+     *
      * @return string error message
      */
     public function getErrorMessage()
@@ -152,4 +150,3 @@ class Ilib_Services_CPSMS
         return $this->errormessage;
     }
 }
-?>
